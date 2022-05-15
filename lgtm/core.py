e3142c2c67cdc2ae902c65a0d1e4d812c6923fc8
au-n <yudai.i1002@gmail.com>
2022-05-15 11:33:38 +0900

    Circlecinokomitto

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,8 +1,12 @@
 import click
 
 @click.command()
-def cli():
-	lgtm()
+@click.option('--message', '-m', default='LGTM',
+	show_default=True, help='画像に乗せる文字')
+@click.argument('keyword')
+
+def cli(keyword, message):
+	lgtm(keyword, message)
 	click.echo('lgtm')
 
 
